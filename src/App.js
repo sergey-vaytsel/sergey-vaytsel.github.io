f25836fb23867883bd9a5c5313372bd72e570b70
Sergey Vaytsel <seralwei@gmail.com>
2021-08-26 17:29:36 +0300

    added contacts

--- src/App.js
+++ src/App.js
@@ -1,17 +1,72 @@
+import React from 'react';
 import './App.css';
 
+class Socials extends React.Component {
+  constructor(props) {
+    super(props);
+    this.data = [
+      {
+        name: "mail",
+        short_name: "@",
+        link: "mailto:sergey.vaytsel@gmail.com"
+      },
+      {
+        name: "linkedin",
+        short_name: "lnkdn",
+        link: "https://www.linkedin.com/in/s-vaytsel"
+      },
+      {
+        name: "github",
+        short_name: "gthb",
+        link: "https://github.com/sergey-vaytsel"
+      },
+      {
+        name: "instagram",
+        short_name: "instgrm",
+        link: "https://www.instagram.com/vaytselsergey"
+      },
+      {
+        name: "goodreads",
+        short_name: "gdrds",
+        link: "https://www.goodreads.com/user/show/88544319-sergey-vaytsel"
+      },
+      {
+        name: "facebook",
+        short_name: "fcbk",
+        link: "https://www.facebook.com/sergey.vaytsel/"
+      }
+    ];
+  }
+
+  render() {
+    return this.data.map(info => {
+      return <a className="contact" id={info.name} href={info.link} title={info.name}><b>{info.short_name}</b></a>;
+    });
+  }
+}
+
 function App() {
+  const intro = "I'm Sergey Vaytsel, a programmer.";
+  const dgis_link = "https://dev.2gis.ru/mobile-sdk";
+  const profile = [
+    <p>Currently I am the 3D Map Team Lead at <b><a id="dgis" href={dgis_link}>2GIS</a></b>.</p>,
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  ];
   return (
     <div className="App container">
       <div className="row intro">
         <div className="column">
-          Hi, I'm Sergey Vaytsel, a programmer.
+          {intro}
         </div>
       </div>
       <div className="row profile">
         <div className="column">
-          <p>Currently I am the <b>3D Map</b> Team Lead at <a className="2gis" href="https://2gis.ru">2GIS</a>.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          {profile.map(paragraph => { return paragraph; })}
+        </div>
+      </div>
+      <div className="row contacts">
+        <div className="column">
+          <Socials></Socials>
         </div>
       </div>
     </div>
