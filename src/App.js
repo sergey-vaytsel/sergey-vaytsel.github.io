e83c16a75eba00178ba5dc5ea2bfad3f2bbcab87
Sergey Vaytsel <seralwei@gmail.com>
2021-08-26 13:54:58 +0300

    fish-app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
+      <div className="row intro">
+        <div className="column">
+          Hi, I'm Sergey Vaytsel, a programmer.
+        </div>
+      </div>
+      <div className="row profile">
+        <div className="column">
+          <p>Currently I am the <b>3D Map</b> Team Lead at <a className="2gis" href="https://2gis.ru">2GIS</a>.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </div>
     </div>
   );
 }
