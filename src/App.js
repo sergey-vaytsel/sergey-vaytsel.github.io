65f5f97c653daeebf2266511550687eb9eb72665
Sergey Vaytsel <seralwei@gmail.com>
2021-09-04 23:45:16 +0300

    added logo on a page, changed contacts colors, fixed contacts wrapping bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { ReactComponent as Logo } from './img.svg';
 
 class Socials extends React.Component {
   constructor(props) {
@@ -7,32 +8,32 @@ class Socials extends React.Component {
     this.data = [
       {
         name: "mail",
-        short_name: "@",
+        short_name: <span>@</span>,
         link: "mailto:sergey.vaytsel@gmail.com"
       },
       {
         name: "linkedin",
-        short_name: "lnkdn",
+        short_name: <span>lnkdn</span>,
         link: "https://www.linkedin.com/in/s-vaytsel"
       },
       {
         name: "github",
-        short_name: "gthb",
+        short_name: <span>gthb</span>,
         link: "https://github.com/sergey-vaytsel"
       },
       {
         name: "instagram",
-        short_name: "instgrm",
+        short_name: <span>instgrm</span>,
         link: "https://www.instagram.com/sergey_vaytsel"
       },
       {
         name: "goodreads",
-        short_name: "gdrds",
+        short_name: <span><span className="goodreads-good">gd</span>rds</span>,
         link: "https://www.goodreads.com/sergey_vaytsel"
       },
       {
         name: "facebook",
-        short_name: "fcbk",
+        short_name: <span>fcbk</span>,
         link: "https://www.facebook.com/sergey.vaytsel"
       }
     ];
@@ -40,12 +41,12 @@ class Socials extends React.Component {
 
   render() {
     return this.data.map(info => {
-      return <a
-          className="contact"
-          id={info.name}
-          href={info.link}
-          title={info.name}
-        ><b>{info.short_name}</b></a>;
+      return <span key={info.name}>
+        <a className="contact" id={info.name}
+           href={info.link} title={info.name}>
+          {info.short_name}
+        </a>&thinsp;
+      </span>;
     });
   }
 }
@@ -54,22 +55,27 @@ function App() {
   const intro = "I'm Sergey Vaytsel, a programmer.";
   const dgis_link = "https://dev.2gis.ru/mobile-sdk";
   const profile = [
-    <p>
+    <p key="1">
       Currently I am the &ldquo;3D Map&rdquo; Team Lead
       at <b><a id="dgis" href={dgis_link}>2GIS</a></b>.
     </p>,
-    <p>
+    <p key="2">
       In different stages of life, I was fond of origami,
       basketball, Japan culture, playing guitar, computer games, mathematics,
       programming, testing, design, typography, writing and investments.
     </p>,
-    <p>
+    <p key="3">
       To contact me you can use contacts below.<br/>
       Now I live in St.&nbsp;Petersburg.
     </p>
   ];
   return (
     <div className="App container">
+      <div className="row logo">
+        <div className="column">
+          <Logo className="logo-svg" />
+        </div>
+      </div>
       <div className="row intro">
         <div className="column">
           {intro}
